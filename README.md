ae8168b39d5f753f17433f5657e4df05bb90fd3e
maciok <maciek1pietrzak@gmail.com>
2018-01-23 21:21:08 +0100

    Added source file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gohttpcounter
+
+Sample Go Lang program with HTTP interface and cookies
---
+++ httpcounter.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+const COUNTER_COOKIE = "pingPongCounter"
+
+func handlePing(w http.ResponseWriter, r *http.Request) {o
+	i := cookieHandler(r, w, func(i int) int {
+		return i + 1
+	})
+	fmt.Fprintf(w, "Pong!\nCountner: %d", i)
+}
+
+func handlePong(w http.ResponseWriter, r *http.Request) {
+	i := cookieHandler(r, w, func(i int) int {
+		return i - 1
+	})
+	fmt.Fprintf(w, "Ping!\nCounter: %d", i)
+}
+
+func cookieHandler(r *http.Request, w http.ResponseWriter, counter func(int) int) int {
+	c, err := r.Cookie(COUNTER_COOKIE)
+	if err == http.ErrNoCookie {
+		c = &http.Cookie{
+			Name:   COUNTER_COOKIE,
+			Value:  "0",
+			MaxAge: 3600,
+		}
+	}
+	i, _ := strconv.Atoi(c.Value)
+	i = counter(i)
+	c.Value = strconv.FormatInt(int64(i), 10)
+	http.SetCookie(w, c)
+	return i
+}
+
+func main() {
+	http.HandleFunc("/ping", handlePing)
+	http.HandleFunc("/pong", handlePong)
+	http.ListenAndServe(":8080", nil)
+}
